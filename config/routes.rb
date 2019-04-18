40eece18c6464b76d58756f7673db4292a368ce2
ian douglas <ian.douglas@iandouglas.com>
2019-04-18 12:37:04 -0600

    no routing, no problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,3 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
-
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
-
-  resources :users, only: [:new, :create, :show]
-
-  resources :carts, only: [:create]
-  resources :orders, only: [:create]
-
-  resources :artists, only: [:index, :show, :new, :create] do
-    resources :songs, only: [:index, :new, :create, :edit, :update]
-  end
-
-  resources :songs, only: [:index, :show]
-
-  namespace :admin do
-    resources :categories, only: [:index]
-  end
 end
