91c12fadcce290cbf9aafd9008e5e83e129a92b7
ian douglas <ian.douglas@iandouglas.com>
2019-04-18 12:50:00 -0600

    add a new song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,21 +7,6 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
   end
 
-  def new
-    @artist = Artist.find(params[:artist_id])
-    @song = Song.new
-  end
-
-  def create
-    @artist = Artist.find(params[:artist_id])
-    @song = @artist.songs.new(song_params)
-    if @song.save
-      redirect_to song_path(@song)
-    else
-      render :new
-    end
-  end
-
   def edit
     @artist = Artist.find(params[:artist_id])
     @song = Song.find(params[:id])
@@ -39,11 +24,5 @@ class SongsController < ApplicationController
     else
       render :edit
     end
-  end
-
-  private
-
-  def song_params
-    params.require(:song).permit(:title, :length, :play_count)
   end
 end
