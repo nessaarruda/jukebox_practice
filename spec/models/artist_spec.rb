7df80238923ab5d1b06a6743a2b6c34e7458108f
ian douglas <ian.douglas@iandouglas.com>
2019-04-18 12:44:06 -0600

    no model testing

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,11 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
   describe 'relationships' do
-    it {should have_many :songs}
   end
 
   describe 'validations' do
-    it {should validate_presence_of :name}
   end
 
   describe 'class methods' do
--- spec/models/playlist_songs_spec.rb
+++ spec/models/playlist_songs_spec.rb
@@ -1,8 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe PlaylistSong, type: :model do
-  describe 'relationships' do
-    it {should belong_to :song}
-    it {should belong_to :playlist}
-  end
 end
